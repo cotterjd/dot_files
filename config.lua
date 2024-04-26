745f9dc1c7980e3e37385f0d646a1274004d1acf
Jordan Cotter <jrdn.cttr@gmail.com>
2024-04-25 22:59:48 -0500

    Create config.lua

---
+++ config.lua
@@ -0,0 +1,11 @@
+-- ~/.config/lvim/config.lua
+-- Read the docs: https://www.lunarvim.org/docs/configuration
+-- Video Tutorials: https://www.youtube.com/watch?v=sFA9kX-Ud_c&list=PLhoH5vyxr6QqGu0i7tt_XoVK9v-KvZ3m6
+-- Forum: https://www.reddit.com/r/lunarvim/
+-- Discord: https://discord.com/invite/Xb9B4Ny
+
+lvim.keys.normal_mode["<leader>="] = ":vertical resize +2<CR>"
+lvim.keys.normal_mode["<leader>-"] = ":vertical resize -2<CR>"
+lvim.keys.normal_mode["<leader>+"] = ":resize +2<CR>"
+lvim.keys.normal_mode["<leader>_"] = ":resize -2<CR>"
+lvim.keys.normal_mode["<tab>"] = ":bn<CR>"
