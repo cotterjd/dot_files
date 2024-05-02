cac068b8acbe3157ec45d1f23b5bceb02c6028ab
Jordan Cotter <jrdn.cttr@gmail.com>
2024-05-02 09:37:28 -0500

    Update config.lua

--- config.lua
+++ config.lua
@@ -4,14 +4,14 @@
 -- Forum: https://www.reddit.com/r/lunarvim/
 -- Discord: https://discord.com/invite/Xb9B4Ny
 
-lvim.keys.normal_mode["<leader>="] = ":vertical resize +2<CR>"
-lvim.keys.normal_mode["<leader>-"] = ":vertical resize -2<CR>"
-lvim.keys.normal_mode["<leader>+"] = ":resize +2<CR>"
-lvim.keys.normal_mode["<leader>_"] = ":resize -2<CR>"
+lvim.keys.normal_mode["<leader>="] = ":vertical resize +2<CR>" -- +
+lvim.keys.normal_mode["<leader>-"] = ":vertical resize -2<CR>" -- -
+lvim.keys.normal_mode["<leader>+"] = ":resize +2<CR>" -- shift +
+lvim.keys.normal_mode["<leader>_"] = ":resize -2<CR>" -- shift -
 lvim.keys.normal_mode["<tab>"] = ":bn<CR>"
 lvim.keys.normal_mode["<leader>p"] = "\"+p" -- past from clipboard
-lvim.keys.normal_mode["<leader>v"] = ":vsp<CR>"
-lvim.keys.normal_mode["<leader>h"] = ":sp<CR>"
+lvim.keys.normal_mode["<leader>v"] = ":vsp<CR>" -- v for vertical
+lvim.keys.normal_mode["<leader>s"] = ":sp<CR>" -- s for split
 lvim.keys.normal_mode["<leader>rn"] = ":set relativenumber<CR>" -- relativenumber is set by default (see below). this is to be used with the explorer
 
 
