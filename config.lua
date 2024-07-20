f43429602360236ca8a3d9140be2457cea56190d
Jordan Cotter <jrdn.cttr@gmail.com>
2024-07-20 05:04:58 -0500

    Update config.lua

--- config.lua
+++ config.lua
@@ -14,12 +14,24 @@ lvim.keys.normal_mode["<leader>v"] = ":vsp<CR>" -- v for vertical
 lvim.keys.normal_mode["<leader>s"] = ":sp<CR>" -- s for split
 lvim.keys.normal_mode["<leader>rn"] = ":set relativenumber<CR>" -- relativenumber is set by default (see below). this is to be used with the explorer
 
-
 vim.opt.relativenumber = true
 vim.opt.foldmethod = "indent"
 
+vim.cmd([[
+  command! -nargs=0 ShowFilePath lua print(vim.api.nvim_buf_get_name(0))
+]])
+
+local function open_tree_and_set_relativenumber()
+    vim.cmd([[NvimTreeToggle]])
+    vim.cmd([[set relativenumber]])
+end
+vim.keymap.set('n', '<leader>e', open_tree_and_set_relativenumber, { remap = true })
+
 lvim.plugins = {
   { "github/copilot.vim" },
+  { "neovim/nvim-lspconfig" },
+  { "jose-elias-alvarez/null-ls.nvim" },
+  { "MunifTanjim/prettier.nvim" },
   {
     "ray-x/lsp_signature.nvim", -- show signature when using function
     event = "VeryLazy",
@@ -27,3 +39,57 @@ lvim.plugins = {
     config = function(_, opts) require'lsp_signature'.setup(opts) end
   }
 }
+
+local null_ls = require("null-ls")
+
+local group = vim.api.nvim_create_augroup("lsp_format_on_save", { clear = false })
+local event = "BufWritePre" -- or "BufWritePost"
+local async = event == "BufWritePost"
+
+null_ls.setup({
+  on_attach = function(client, bufnr)
+    if client.supports_method("textDocument/formatting") then
+      -- vim.keymap.set("n", "<Leader>r", function()
+      --   vim.lsp.buf.format({ bufnr = vim.api.nvim_get_current_buf() })
+      -- end, { buffer = bufnr, desc = "[lsp] format" })
+
+      -- format on save
+      vim.api.nvim_clear_autocmds({ buffer = bufnr, group = group })
+      vim.api.nvim_create_autocmd(event, {
+        buffer = bufnr,
+        group = group,
+        callback = function()
+          vim.lsp.buf.format({ bufnr = bufnr, async = async })
+        end,
+        desc = "[lsp] format on save",
+      })
+    end
+
+    -- if client.supports_method("textDocument/rangeFormatting") then
+    --   vim.keymap.set("x", "<Leader>r", function()
+    --     vim.lsp.buf.format({ bufnr = vim.api.nvim_get_current_buf() })
+    --   end, { buffer = bufnr, desc = "[lsp] format" })
+    -- end
+  end,
+})
+
+local prettier = require("prettier")
+
+prettier.setup({
+  bin = 'prettier', -- or `'prettierd'` (v0.23.3+)
+  filetypes = {
+    "css",
+    "graphql",
+    "html",
+    "javascript",
+    "javascriptreact",
+    "json",
+    "less",
+    "markdown",
+    "scss",
+    "typescript",
+    "typescriptreact",
+    "yaml",
+  },
+})
+
